493d7222311d923b2ea6addc785c48e5308c8336
Saeid Mohadjer <saeid@fastmail.fm>
2023-03-17 18:09:06 +0100

    update

--- api/login.js
+++ api/login.js
@@ -1,8 +1,48 @@
+import uri from './db.js';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(uri);
+
+async function run(username, password) {
+  try {
+    console.log('openning db...');
+    await client.connect();
+    const database = client.db('test');
+    const users = database.collection('members');
+    const query = { username: username, password: password };
+    const options = {
+      // Include only username field in the returned document
+      projection: { _id: 0, username: 1 },
+    };
+
+    const result = await users.findOne(query, {options});
+
+   if (result) {
+     return true;
+   } else {
+    return false;
+   }
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log('closing db...');
+    //Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
 export default async (req, res) => {
   if (req.body) {
-    console.log('got body');
+    const { username, password } = req.body;
+
+    const userFound = await run(username, password);
+
     res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
+    if (userFound) {
+      res.end('Login Succeeded!');
+    } else {
+      res.end('Login Failed!');
+    }
   }
 }
--- api/register.js
+++ api/register.js
@@ -1,8 +1,29 @@
-export default (req, res) => {
-  if (req.body) {
-    console.log('got body');
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('you posted:\n')
-    res.end(JSON.stringify(req.body, null, 2))
+import uri from './db.js';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(uri);
+
+async function run(req) {
+  try {
+    console.log('openning db...');
+    await client.connect();
+    const database = client.db('test');
+    const users = database.collection('members');
+    await users.insertOne({
+      username: req.body.username.trim(),
+      password: req.body.password.trim(),
+    });
+  } catch (e) {
+    console.error(e);
+  } finally {
+    console.log('closing db...');
+    //Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
+
+export default async (req, res) => {
+    await run(req);
+    return res.send('You are now registered.');
+}
+
